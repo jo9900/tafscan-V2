6948919478ba7551a479e1872ad20268abe19ea9
shinkai76 <249333013@qq.com>
2021-10-26 15:20:49 +0800

    .prettierrc

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,8 @@
-const path = require('path');
+const path = require('path')
 const CompressionWebpackPlugin = require('compression-webpack-plugin')
-const resolve = dir => path.join(__dirname, dir);
-
+const resolve = dir => path.join(__dirname, dir)
 const sign = process.argv[4] // pc or  m
-
-// 是否使用gzip
 const productionGzip = true
-// 需要gzip压缩的文件后缀
 const productionGzipExtensions = ['js', 'css']
 
 const pages = {
@@ -14,15 +10,15 @@ const pages = {
     entry: 'src/pages/m/main.ts',
     template: 'src/pages/m/index.html',
     filename: 'index.html',
-    title: 'Index Page',
-    chunks: ['chunk-vendors', 'chunk-common', 'm']
+    title: 'm',
+    chunks: ['chunk-vendors', 'chunk-common', 'm'],
   },
   pc: {
     entry: 'src/pages/pc/main.ts',
     template: 'src/pages/pc/index.html',
     filename: 'index.html',
-    title: 'Index Page PC',
-    chunks: ['chunk-vendors', 'chunk-common', 'pc']
+    title: 'pc',
+    chunks: ['chunk-vendors', 'chunk-common', 'pc'],
   },
 }
 
@@ -32,7 +28,7 @@ module.exports = {
   filenameHashing: true,
   outputDir: 'dist-' + sign,
   pages: {
-    [sign]: pages[sign]
+    [sign]: pages[sign],
   },
   chainWebpack: config => {
     config.resolve.alias
@@ -43,22 +39,12 @@ module.exports = {
     config.plugins.delete('named-chunks')
 
     if (process.env.NODE_ENV !== 'development') {
-      // 启用GZip压缩
-      productionGzip && new CompressionWebpackPlugin({
-        test: new RegExp('\\.(' + productionGzipExtensions.join('|') + ')$'),
-        threshold: 8192,
-        minRatio: 0.8
-      })
+      productionGzip &&
+        new CompressionWebpackPlugin({
+          test: new RegExp('\\.(' + productionGzipExtensions.join('|') + ')$'),
+          threshold: 8192,
+          minRatio: 0.8,
+        })
     }
   },
-  configureWebpack: config => {
-    if (process.env.NODE_ENV === 'production') {
-      config.output = {
-        path: path.join(__dirname, './dist'),
-        filename: '[name]/js/[name].[contenthash:8].js',
-        publicPath: '/',
-        chunkFilename: '[name]/js/[name].[contenthash:8].js'
-      };
-    }
-  }
 }
